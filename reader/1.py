cb182ff0a318c9b98badef8d4454814803dc2844
dudu7615 <dudu7615@qq.com>
2022-06-18 20:06:47 +0800

    0

--- reader/1.py
+++ reader/1.py
@@ -5,25 +5,26 @@ from tkinter import filedialog
 speaker = win32com.client.Dispatch("SAPI.SpVoice").Speak
 
 
-root = Tk()
-root.geometry('300x70')
-root.title("Reader")
+app = Tk()
+app.geometry('280x70')
+app.title("Reader")
 
-def read():
-    speaker(r2.get())
 
-def txt():
+def read_text():
+    speaker(r3.get("1.0","end"))
+
+def read_file():
     name = filedialog.askopenfilename(filetypes=[("文本文件",".txt")])
     file1 = open(name,'r',encoding='utf8')
-    speaker(file1.read())
+    r3.insert(1.0,file1.read())
+   
+r1 = Button(app,text="开始朗读",font=("微软雅黑",8),fg="green",command=read_text)
+r2 = Button(app,text="浏览文件",font=("微软雅黑",8),fg="green",command=read_file)
+r3 = Text(app,font=("微软雅黑",10))
 
-r1 = Button(root,text="开始朗读",font=("微软雅黑",8),fg="green",command=read)
-r1.place(x=240,y=35,height=25, width=50)
+r1.place(x=220,y=10,height=25, width=50)
+r2.place(x=220,y=35,height=25, width=50)
+r3.place(x=10,y=10,height=50, width=200)
 
-r2 = Entry(root,text="",font=("微软雅黑",15))
-r2.place(x=10,y=10,height=50, width=220)
+app.mainloop()
 
-r3 = Button(root,text="浏览文件",font=("微软雅黑",8),fg="green",command=txt)
-r3.place(x=240,y=10,height=25, width=50)
-
-root.mainloop()
